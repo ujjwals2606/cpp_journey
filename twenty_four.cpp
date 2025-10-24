e9650f8a2883c7c36d4948874a6318c452fe670c
Ujjwal Singh <156454453+ujjwals2606@users.noreply.github.com>
2025-10-24 16:31:15 +0530

    Add program to input and display age

---
+++ twenty_four.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+
+    // Taking input from user and store it in variable
+    cin >> age;
+
+    // Output the entered age
+    cout << "Age entered: " << age;
+    
+    return 0;
+}
