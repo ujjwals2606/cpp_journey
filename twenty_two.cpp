e315a3d95c72e72431621f539ef293b60de5b237
Ujjwal Singh <156454453+ujjwals2606@users.noreply.github.com>
2025-10-24 16:22:03 +0530

    Add program to find the greatest of two numbers

---
+++ twenty_two.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3, b = 4;
+
+    // Conditional Operator
+    int result = (a < b) ? b : a;
+    cout << "The greatest number "
+          "is " << result;
+
+    return 0;
+}
