2ffb00a827366e8a9bd7593614335df3a9e93985
Ujjwal Singh <156454453+ujjwals2606@users.noreply.github.com>
2025-10-24 15:13:44 +0530

    Add program to display sizes of data types
    This program prints the size of various data types in C++.

---
+++ ten.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // Printing the size of each data type
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes";
+
+    return 0;
+}
