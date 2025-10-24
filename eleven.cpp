d3f069242e69d57fe19f00f777361763cd64de8d
Ujjwal Singh <156454453+ujjwals2606@users.noreply.github.com>
2025-10-24 15:15:33 +0530

    Add basic C++ program to demonstrate variable usage

---
+++ eleven.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    // Creating a single character variable
+    int num = 3;
+    
+    // Accessing and printing above variable
+    cout << num << endl;
+    
+    // Update the value
+    num = 7;
+    
+    // Printing the updated value
+    cout << num;
+    
+    return 0;
+}
