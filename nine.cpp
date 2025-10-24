b8ad4d85c32a53e2de7d3a055ab4af9948bb7685
Ujjwal Singh <156454453+ujjwals2606@users.noreply.github.com>
2025-10-24 15:12:36 +0530

    Add main function to convert char to int and sum
    Implement main function to demonstrate char to int conversion and sum.

---
+++ nine.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 3;
+    char c = 'C';
+
+    // Convert char data type into integer
+    cout << (int)c << endl;
+    
+    int sum = n + c;
+    cout << sum;
+    return 0;
+}
