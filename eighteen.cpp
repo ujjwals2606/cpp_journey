031dfb4547acccaaed9c3f5368e17ccf2e59195a
Ujjwal Singh <156454453+ujjwals2606@users.noreply.github.com>
2025-10-24 16:14:33 +0530

    Implement comparison operators in eighteen.cpp
    Demonstrate various comparison operators in C++.

---
+++ eighteen.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 6, b = 4;
+
+    // Equal operator
+    cout << "a == b is " << (a == b) << endl;
+  
+    // Greater than operator
+    cout << "a > b is " << (a > b) << endl;
+  
+    // Greater than Equal to operator
+    cout << "a >= b is " << (a >= b) << endl;
+  
+    //  Lesser than operator
+    cout << "a < b is " << (a < b) << endl;
+  
+    // Lesser than Equal to operator
+    cout << "a <= b is " << (a <= b) << endl;
+  
+    // Not equal to operator
+    cout << "a != b is " << (a != b);
+
+    return 0;
+}
