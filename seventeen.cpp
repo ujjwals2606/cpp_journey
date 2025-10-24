2da078546171fb70e0c0bce93200d825c060591d
Ujjwal Singh <156454453+ujjwals2606@users.noreply.github.com>
2025-10-24 16:12:47 +0530

    Implement basic arithmetic operations in C++

---
+++ seventeen.cpp
@@ -0,0 +1,29 @@
+  #include <iostream>
+using namespace std;
+
+int main() {
+    int a = 8, b = 3;
+
+    // Addition
+    cout << "a + b = " << (a + b) << endl;
+  
+    // Subtraction
+    cout << "a - b = " << (a - b) << endl;
+  
+    // Multiplication
+    cout << "a * b = " << (a * b) << endl;
+  
+    // Division
+    cout << "a / b = " << (a / b) << endl;
+  
+    // Modulo
+    cout << "a % b = " << (a % b) << endl;
+  
+    // Increament
+    cout << "++a = " << ++a << endl;
+  
+    // Decrement
+    cout << "b-- = " << b--;
+    
+    return 0;
+}
