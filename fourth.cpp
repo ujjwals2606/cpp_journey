71edea4e8ca993aa280662b4e38ce64519fd4236
Ujjwal Singh <156454453+ujjwals2606@users.noreply.github.com>
2025-10-24 15:06:46 +0530

    Add fourth.cpp with integer and hex output example
    Demonstrate integer variable and hexadecimal output

---
+++ fourth.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // Creating an integer variable
+    int x = 25;
+    cout << x << endl;
+
+    // Using hexadecimal base value
+    x = 0x15;
+    cout << x;
+
+    return 0;
+}
