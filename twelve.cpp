eefff0c8fffd3a786b367d431fb836e791f1beb5
Ujjwal Singh <156454453+ujjwals2606@users.noreply.github.com>
2025-10-24 15:19:21 +0530

    Add twelve.cpp to demonstrate variable assignment

---
+++ twelve.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 10, num2;
+    
+    // Assigning num1's value to num2
+    num2 = num1;
+    cout << num1 << " " << num2;
+    return 0;
+}
